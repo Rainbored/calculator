e889901530cdbb8f67e940222ef0d53088e7d9e4
Rain <whatismyname037@gmail.com>
2025-08-31 20:33:39 +0600

    added and styled btns

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import "./index.css";
+import "./app.css";
+
 export default function App() {
+  const smth = [
+    'C', '()', '%', '/',
+     7,   8,   9,   'X', 
+     4,   5,   6,   '-',
+     1,   2,   3,   '+',
+    '',   0,  '.',  '='
+    ]
+
   return (
     <>
-    
+    <div id="calculator">
+      <div id="btn-container">
+        {smth.map((btn) => <button key={btn} type="button">{btn}</button>)}
+      </div>
+    </div>
     </>
   );
 }
